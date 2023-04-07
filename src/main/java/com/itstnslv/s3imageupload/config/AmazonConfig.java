8f2953f9e7ce517c731df3f20babac1f68a32966
itstnslv <cancion2able@gmail.com>
2023-04-07 16:32:40 +0600

    Provide amazon config

---
+++ src/main/java/com/itstnslv/s3imageupload/config/AmazonConfig.java
@@ -0,0 +1,36 @@
+package com.itstnslv.s3imageupload.config;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+
+@Configuration
+@PropertySource("classpath:application.properties")
+public class AmazonConfig {
+
+    @Autowired
+    private Environment env;
+
+    @Value("${access.key}")
+    private String accessKey;
+
+    @Value("${access.secret}")
+    private String accessSecret;
+
+    @Bean
+    public AmazonS3 s3() {
+        BasicAWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, accessSecret);
+        return AmazonS3ClientBuilder
+                .standard()
+                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+                .build();
+    }
+
+}
