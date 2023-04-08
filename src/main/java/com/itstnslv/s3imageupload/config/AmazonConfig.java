0099e571801dd3e50ef4bdc0f1209eda4a997dff
itstnslv <cancion2able@gmail.com>
2023-04-08 16:16:59 +0600

    Setup AWS connection

--- src/main/java/com/itstnslv/s3imageupload/config/AmazonConfig.java
+++ src/main/java/com/itstnslv/s3imageupload/config/AmazonConfig.java
@@ -2,19 +2,20 @@ package com.itstnslv.s3imageupload.config;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 
 @Configuration
+@PropertySource("classpath:application.properties")
 public class AmazonConfig {
 
-    @Autowired
-    private Environment env;
+    private final Environment env;
 
     @Value("${aws.access.key}")
     private String accessKey;
@@ -22,12 +23,20 @@ public class AmazonConfig {
     @Value("${aws.access.secret}")
     private String accessSecret;
 
+    public AmazonConfig(Environment env) {
+        this.env = env;
+    }
+
     @Bean
     public AmazonS3 s3() {
-        BasicAWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, accessSecret);
+        BasicAWSCredentials awsCredentials = new BasicAWSCredentials(
+                System.getenv(accessKey),
+                System.getenv(accessSecret)
+        );
         return AmazonS3ClientBuilder
                 .standard()
                 .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+                .withRegion(Regions.DEFAULT_REGION)
                 .build();
     }
 
